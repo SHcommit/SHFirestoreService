178c4cc9cd92004ffea89d37b8ca73660df0b3e9
shcommit <happysh_s2@naver.com>
2024-05-01 23:14:47 +0900

    [Feat] Add FirestoreMethod annotation and getDocumentIdList case

--- Sources/SHFirestoreService/FirestoreMethod.swift
+++ Sources/SHFirestoreService/FirestoreMethod.swift
@@ -10,7 +10,15 @@ import FirebaseFirestore
 
 @frozen public enum FirestoreMethod {
   public typealias DocumentId = String
+  
+  /// Get a list of document IDs in a specific collection.
+  ///   At this time, the document ID of the document is retrieved regardless of whether there is no field for the document.
+  case getDocumentIdList
+  
+  /// Decodes the documents in a specific collection and returns the desired Decodable.
+  /// However, even if a field in the document does not exist, decoding may result in an error indicating that the document cannot be found.
   case get
+  
   /// If you want to specify the document name directly, just enter the associate value.
   case save(DocumentId?)
   case delete
