c69dff8d443e3c4cacd69586fb5e6f2f4141f5b3
shcommit <happysh_s2@naver.com>
2024-05-08 23:55:44 +0900

    [Style] Comment example about commit() in Combine+WriteBatch.swift

--- Sources/SHFirestoreService/Combine+WriteBatch.swift
+++ Sources/SHFirestoreService/Combine+WriteBatch.swift
@@ -14,7 +14,26 @@ import Combine
 @available(swift 5.0)
 @available(iOS 13.0, *)
 
-extension Publisher where Output == WriteBatch, Failure == FirestoreServiceError {
+public extension Publisher where Output == WriteBatch, Failure == FirestoreServiceError {
+  
+  /// This is an extension function used to commit WriteBatches.
+  ///
+  /// Notes:
+  /// 1. Generate and return a WriteBatch from the upstream publihser's output value.
+  /// 2. Perform to commit batch using tins func
+  ///
+  /// Examples:
+  /// ```
+  /// # using the batch and commit functions when deleting an entire document #
+  /// let collectionRef = ... # Get CollectionReference..
+  /// let subscription = collectionRef.getDocuments().map { snapshot in
+  ///   let batch = Firestore.firestore().batch()
+  ///   snapshot.documents.forEach { batch.deleteDocument($0.reference) }
+  ///   return batch
+  /// }
+  /// .commit() # perform commit from upstream's a batch : ]
+  ///           # downstream
+  /// ```
   func commit() -> AnyPublisher<Void, Failure> {
     return self.flatMap { batch in
       return batch
