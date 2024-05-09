fd7d91af6173a02c02dea3c509147cca70168212
shcommit <happysh_s2@naver.com>
2024-05-09 14:15:14 +0900

    Feat] Add retrieveNumberOfDocuments case in FirestoreMethod

--- Sources/SHFirestoreService/FirestoreMethod.swift
+++ Sources/SHFirestoreService/FirestoreMethod.swift
@@ -15,6 +15,12 @@ import FirebaseFirestore
   ///   At this time, the document ID of the document is retrieved regardless of whether there is no field for the document.
   case retrieveDocumentIdList
   
+  /// Retrieves the number of all documents in a specified collectionRef from FirestoreAccessible.
+  /// Note:
+  /// 1. This method using AggrateQuery's count() that match between 0 and 1000 index entries billed for one documen read: ]
+  /// https://firebase.google.com/docs/firestore/query-data/aggregation-queries
+  case retrieveNumberOfDocuments
+  
   /// Decodes the documents in a specific collection and returns the desired Decodable.
   /// However, even if a field in the document does not exist, decoding may result in an error indicating that the document cannot be found.
   case get
